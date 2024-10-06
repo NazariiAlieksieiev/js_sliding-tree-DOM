da5a6b8dee6288f944ca7743ed0fcc8dc3077c34
Nazarii AlieKsieiev <nazarmetall@gmail.com>
2024-10-06 22:30:17 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,20 @@
+"use strict";
+const itemsOfList = [
+    ...document.querySelectorAll("li")
+];
+itemsOfList.forEach((item)=>{
+    if (item.children.length) {
+        const span = document.createElement("span");
+        span.textContent = item.childNodes[0].textContent.trim();
+        item.prepend(span);
+        item.childNodes[1].textContent = "";
+        let hidden = false;
+        span.onclick = (e)=>{
+            e.preventDefault();
+            span.nextElementSibling.style.display = hidden ? "block" : "none";
+            hidden = !hidden;
+        };
+    }
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
